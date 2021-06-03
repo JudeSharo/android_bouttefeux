a0ee24dc927f457f309f048280c431bbef632512
Gogoth <hugo.chauviere2000@gmail.com>
2021-06-03 16:42:54 +0200

    page contact

--- app/src/main/java/com/example/bouttefeux/ContactActivity.java
+++ app/src/main/java/com/example/bouttefeux/ContactActivity.java
@@ -2,13 +2,17 @@ package com.example.bouttefeux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
     private Button BT1;
+    private TextView lienFB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,7 @@ public class ContactActivity extends AppCompatActivity {
     protected void initialisation()
     {
         BT1 = findViewById(R.id.contact_BT1);
+        lienFB = findViewById(R.id.lienFacebook);
     }
 
     protected void defaut(){}
@@ -35,6 +40,25 @@ public class ContactActivity extends AppCompatActivity {
 
             }
         });
+
+        lienFB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lienFacebook();
+            }
+
+        });
+    }
+
+    private void lienFacebook() {
+        try{
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Bouttefeux-yohan-321817244628825"));
+            startActivity(intent);
+        }
+        catch(ActivityNotFoundException e){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com"));
+            startActivity(intent);
+        }
     }
 
 }
